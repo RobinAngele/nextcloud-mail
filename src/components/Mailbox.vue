78e2874f1f3fb672daa48440b26b378c29a46c2e
RobinAngele <robin@robin4consulting.com>
2026-05-07 10:23:37 +0200

    ux: simplify hint - replace technical jargon
    Changed 'click an avatar to pick individually' to
    'select messages one by one' for clarity.
    
    Signed-off-by: RobinAngele <robin@robin4consulting.com>

--- src/components/Mailbox.vue
+++ src/components/Mailbox.vue
@@ -296,7 +296,7 @@ export default {
 		selectAllHint() {
 			// Show hint when more messages exist beyond the loaded page
 			if (!this.endReached && this.flatEnvelopeList.length > 0) {
-				return this.t('mail', 'Scroll down to load more, use filter for custom selection, or click an avatar to pick individually')
+				return this.t('mail', 'Scroll down to load more, use filter for custom selection, or select messages one by one')
 			}
 			return ''
 		},
